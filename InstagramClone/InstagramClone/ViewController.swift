70ee7cde11fd6c268358509010f4bf12d1273170
yaseminsalann <yaseminsalann@gmail.com>
2025-02-11 01:20:37 +0300

    InstagramClone
    InstagramClone

---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Yasemin salan on 11.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InstagramClone/InstagramCloneTests/InstagramCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneTests.swift
+//  InstagramCloneTests
+//
+//  Created by Yasemin salan on 11.02.2025.
+//
+
+import Testing
+@testable import InstagramClone
+
+struct InstagramCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
